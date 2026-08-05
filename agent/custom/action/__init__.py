7a620c517333b1695a20552d6db93b5a3cdfcee6
mukelongji <54195094+Coxwtwo@users.noreply.github.com>
2026-08-05 23:14:58 +0800

    refactor: 移除未使用的 HitsLimiter 类及相关引用

--- agent/custom/action/__init__.py
+++ agent/custom/action/__init__.py
@@ -1,10 +1,8 @@
-from .hits_limiter import *
 from .smart_replenish import *
 from .dynamic_override import *
 from .periodic_task import *
 
 __all__ = [
-    "HitsLimiter",
     "SmartReplenish",
     "DynamicOverride",
     "JudgeDailyTask",
